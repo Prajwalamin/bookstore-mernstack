7a8bbcbfb53cc1d208fb16986c97e2357c6963c9
Prajwal Amin <42278236+Prajwalamin@users.noreply.github.com>
2023-11-21 18:59:47 +0000

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL = 
-    'mongodb+srv://prajwal_amin:manutd23@books-store-mern.94gcv7m.mongodb.net/books-collection?retryWrites=true&w=majority'
+    'mongodb+srv://<username>:<password>@books-store-mern.94gcv7m.mongodb.net/books-collection?retryWrites=true&w=majority'
